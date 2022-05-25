e66f8fd109e3722073f8dce8968cc51fe4319cce
John Russo <John@orlandospencer.com>
2022-05-25 17:57:19 -0400

    Initial Commit

---
+++ Search Up/Search_UpApp.swift
@@ -0,0 +1,17 @@
+//
+//  Search_UpApp.swift
+//  Search Up
+//
+//  Created by John Russo on 5/25/22.
+//
+
+import SwiftUI
+
+@main
+struct Search_UpApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
